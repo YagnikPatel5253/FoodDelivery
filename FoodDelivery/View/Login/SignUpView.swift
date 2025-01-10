6ca3db9ddfe8da2996f6a1df3e208c8952610961
YagnikPatel5253 <yagnikpatel5253@gmail.com>
2025-01-10 14:54:53 +0530

    Commit

---
+++ FoodDelivery/View/Login/SignUpView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpView.swift
+//  FoodDelivery
+//
+//  Created by yagnik on 10/01/25.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpView()
+}
